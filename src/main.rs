9bb6baf3352250268f02a156c2773fe8b026d812
spicydogwings <spaceprotondev@proton.me>
2025-07-09 21:08:37 -0600

    feat(main): add clap support and implement command of kitty sessions - import clap uses - create clap param - create command to execute

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::Parser;
+use std::process::Command;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    // params structure
+    #[arg(short, long)]
+    session: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse(); // params
+    let session_file = args.session + ".conf"; // session file
+    let output = Command::new("nohup")
+        .arg("kitty")
+        .arg("--session")
+        .arg(session_file)
+        .stdout(std::process::Stdio::null())
+        .stderr(std::process::Stdio::null())
+        .spawn() // detach the process
+        .expect("Failed to execute kitty command");
+    println!("{:?}", output);
 }
